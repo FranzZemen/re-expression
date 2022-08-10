baf6d5ab1001c7c72b8ec48a6ead7c68e1882d5d
Franz Zemen <franzzemen@gmail.com>
2022-08-10 12:29:06 -0400

    update

--- ts-src/parser/external-index.ts
+++ ts-src/parser/external-index.ts
@@ -2,7 +2,6 @@ export * from './expression-parser';
 export * from './expression-stack-parser';
 export * from './attribute-expression-parser';
 export * from './function-expression-parser';
-export * from './logical-expression-parser';
 export * from './multivariate-parser';
 export * from './set-expression-parser';
 export * from './value-expression-parser';
--- ts-src/standard/external-index.ts
+++ ts-src/standard/external-index.ts
@@ -4,5 +4,4 @@ export * from './value-expression';
 export * from './function-expression';
 export * from './set-expression';
 export * from './formula-expression';
-export * from './logical-expression';
 export * from './multivariate-expression';
