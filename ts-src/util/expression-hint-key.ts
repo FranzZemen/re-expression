f968d73fe7c7ffc113e6ffd3f6a89fa85f529f0e
Franz Zemen <franzzemen@gmail.com>
2022-08-22 08:14:26 -0400

    hint constant

--- ts-src/util/expression-hint-key.ts
+++ ts-src/util/expression-hint-key.ts
@@ -2,6 +2,6 @@ import {DataTypeHintKey} from '@franzzemen/re-data-type';
 
 export class ExpressionHintKey extends DataTypeHintKey {
   public static Expression = 'ex';
-  public static ExpressionType = 'expression-type';
+  public static ExpressionType = 'type';
   public static Multivariate = 'multivariate';
 }
