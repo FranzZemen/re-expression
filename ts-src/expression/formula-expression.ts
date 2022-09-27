5fc4bf037cd3acd31636ada8127d67eb3167ced7
Franz Zemen <franzzemen@gmail.com>
2022-09-27 06:40:55 -0400

    update

--- ts-src/expression/formula-expression.ts
+++ ts-src/expression/formula-expression.ts
@@ -1,6 +1,8 @@
-import {ModuleDefinition} from '@franzzemen/app-utility';
-import {RecursiveGrouping} from '@franzzemen/re-common';
+import {ExecutionContextI, ModuleDefinition} from '@franzzemen/app-utility';
+import {isRecursiveGrouping, RecursiveGrouping} from '@franzzemen/re-common';
+import {Expression} from '../../publish/index.js';
 import {ExpressionReference, ExpressionType} from '../expression.js';
+import {ExpressionScope} from '../scope/expression-scope.js';
 
 
 export enum FormulaOperator {
@@ -14,48 +16,55 @@ export function isFormulaExpressionReference(ref: any | FormulaExpressionReferen
   return ref['type'] === ExpressionType.Formula && 'operator' in ref && 'group' in ref;
 }
 
+
+export class FormulaElement {
+  constructor() {
+  }
+  operator: FormulaOperator;
+  expression:  Expression;
+}
+
+export class Formula {
+  operator: FormulaOperator;
+  elements: (FormulaOperator | FormulaElement) [] = [];
+
+  constructor() {
+  }
+}
+
+
+
+
+
 export interface FormulaExpressionReference extends RecursiveGrouping<FormulaOperator, ExpressionReference>, ExpressionReference {
   refName?: string;
   // module?: ModuleDefinition;
 }
 
 
-``
+class FormulaExpression extends Expression {
+  refName?: string;
+  formula: Formula = new Formula();
 
-
-/*
-
-
-
-export type BinaryOperator = Operator.Add | Operator.Subtract | Operator.Multiply | Operator.Divide;
-
-export const binaryOperators = [Operator.Add, Operator.Subtract, Operator.Multiply, Operator.Divide];
-
-export interface OperationFragment {
-  binaryOperator?: BinaryOperator;
-  expressionRef: ExpressionReference;
-}
-
-export interface FormulaExpressionReference extends ExpressionReference {
-  operations: OperationFragment[];
-}
-
-export class FormulaExpression extends Expression {
-
-  constructor(ref: FormulaExpressionReference, scope: ExpressionScope, ec?: ExecutionContextI) {
+  constructor(ref: FormulaExpressionReference, scope?: ExpressionScope, ec?: ExecutionContextI) {
     super(ref, scope, ec);
+    this.refName = ref. refName
+    this.formula.operator = ref.operator;
+    ref.group.forEach(item => {
+      if(isRecursiveGrouping(item)) {
+        const formula = new Formula();
+        formula.operator = item.operator;
+        this.formula no..need to recurse!!!
+      }
+    })
   }
+
 
   awaitEvaluation(dataDomain: any, scope: Map<string, any>, ec?: ExecutionContextI): any {
   }
 
-  to(ec?: ExecutionContextI): FormulaExpressionReference {
-    return undefined;
-  }
-
-  protected initializeExpression(scope: ExpressionScope, ec?: ExecutionContextI): FormulaExpression | Promise<FormulaExpression> {
+  to(ec?: ExecutionContextI): FormulaExpression {
     return undefined;
   }
 
 }
-*/
